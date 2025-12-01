80f1d7e154d294b4365f9e241c38f95dab6a5be2
Matthew on office mac studio <myk@macstudio.office>
2025-12-01 20:53:01 +0000

    added new classes

---
+++ src/MarkovModelCPP/build/CMakeFiles/markov-lib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/markov-lib.dir/src/MarkovChain.cpp.o"
+  "CMakeFiles/markov-lib.dir/src/MarkovChain.cpp.o.d"
+  "CMakeFiles/markov-lib.dir/src/MarkovManager.cpp.o"
+  "CMakeFiles/markov-lib.dir/src/MarkovManager.cpp.o.d"
+  "libmarkov-lib.a"
+  "libmarkov-lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/markov-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
